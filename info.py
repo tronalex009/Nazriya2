946dbdbb476022fa973b9e08f7044ef289ce9645
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-03-27 09:36:45 -0500

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ BOT_START_TIME = time()
 
 # Admins, Channels & Users
 ADMINS = [1132901778]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001963801216).split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001963801216').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
