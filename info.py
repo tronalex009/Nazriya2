21caa78cd0d10b183c9b72314272c487933c0071
tronalex009 <111103782+tronalex009@users.noreply.github.com>
2023-03-27 12:01:24 -0500

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ AUTH_CHANNEL = -1001963801216
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://imdb4gb1:imdb4gb1@cluster0.ovlqoai.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://imdb4gb1:imdb4gb1@cluster0.o84hlqoai.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
